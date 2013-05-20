b46677f59b3d807ee3b66cf8ae4c2236d1fe2e1e
Luis Uicab <Shironeko1910@gmail.com>
2013-05-20 17:54:52 -0500

    Aceptación de Tareas Sin Campos a Llenar

--- AgregarCampos.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "ControladorPrincipal.php";
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-    ControladorPrincipal::getInstance()->registraFormulario();
-    echo "hola"
-?>
